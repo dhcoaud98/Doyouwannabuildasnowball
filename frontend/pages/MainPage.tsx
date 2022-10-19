48af2e84b549306ec2d833635a2c230782f3bb0c
오채명 <dhcoaud98@naver.com>
2022-10-19 15:42:22 +0900

    setting : 배경 이미지

---
+++ frontend/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import Button from '@mui/material/Button';
+
+const MainPage= () => {
+  return (
+      <div>
+        <h1>MainPage</h1>
+        <p>여기는 제일 기본이 되는 MainPage입니다.</p>
+
+        <Button>KakaoLogin</Button>
+      </div>
+    )
+}
+
+export default MainPage
