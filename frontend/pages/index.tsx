19142493728d4365660f391f34bb8eaeeb61bb40
오채명 <dhcoaud98@naver.com>
2022-10-19 14:53:58 +0900

    setting : 배경색 넣어보기

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,11 @@
 import Button from '@mui/material/Button';
-
+import styles from "styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>MainPage</h1>
       <p>여기는 제일 기본이 되는 MainPage입니다.</p>
-
       <Button>KakaoLogin</Button>
     </div>
   )
