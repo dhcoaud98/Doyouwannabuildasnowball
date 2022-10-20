c41296310c0ce542c12c17cac4f245eeb60761f3
BaekSeungHoon <psh104404@naver.com>
2022-10-20 16:51:06 +0900

    [Backend] Add Spring Security Configuration

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/config/security/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/config/security/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.ssafy.doyouwannabuildasnowball.config.security;
 
+import com.ssafy.doyouwannabuildasnowball.config.security.oauth.handler.CustomAccessDeniedHandler;
+import com.ssafy.doyouwannabuildasnowball.config.security.oauth.handler.CustomAuthenticationEntryPoint;
+import com.ssafy.doyouwannabuildasnowball.config.security.repository.CookieAuthorizationRequestRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     // 로그인 필요 없는 페이지
+
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
+    private final CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
     private static final String[] PERMIT_URL_ARRAY = {
             /* swagger v2 */
             "/v2/api-docs",
@@ -23,12 +34,64 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/swagger-ui/**",
     };
 
+    /**
+     * Spring Security의 설정 가능
+     *
+     * spring security의 각종 설정은 HTTP Security를 통해 설정
+     * - 리소스(URL) 접근 권한 설정
+     * - 인증 전체 흐름에 힐요한 Login, Logout 페이지 인증 완료 후 페이지 인증 실패 시 이동페이지 등 설정
+     * - 인증 로직을 커스텀하기 위한 커스텀 필터 설정
+     * - 기타 csrf, 강제 https 호출
+     *
+     *
+     * @param http the {@link HttpSecurity} to modify
+     * @throws Exception
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable() // swagger API 호출시 403 에러 발생 방지
                 .authorizeRequests()
-                .antMatchers(PERMIT_URL_ARRAY).permitAll()
-                .anyRequest().anonymous();
+                .antMatchers(PERMIT_URL_ARRAY).permitAll() // 리소스(URL)의 권한 설정, antMatchers 설정한 리소스의 접근을 인증절차 없이 허용
+                .anyRequest().anonymous()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
+                .and()
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("oauth2/authorize")
+                .authorizationRequestRepository(cookieAuthorizationRequestRepository)
+                .and()
+                .redirectionEndpoint()
+                .baseUri("oauth2/code/*")
+//                .and()
+//                .userInfoEndpoint()
+//                .userService(customOAuth2UserService)
+//                .and()
+//                .successHandler()
+//                .failureHandler();
+
+        ;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+//        configuration.addAllowedOrigin("http://localhost:3000");
+//        configuration.addAllowedOrigin("http://localhost:8080");
+//        configuration.addAllowedOrigin("https://i7a601.p.ssafy.io");
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
