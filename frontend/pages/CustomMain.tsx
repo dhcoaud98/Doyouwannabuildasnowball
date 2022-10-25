79c33b58fe8e15c00843d88bbd89b6cd518a27b2
남성은 <myaeja200@naver.com>
2022-10-25 16:58:11 +0900

    feat: 폰트 추가와 3D 배경 추가

--- frontend/pages/CustomMain.tsx
+++ frontend/pages/CustomMain.tsx
@@ -1,6 +1,7 @@
 import { Grid, Stack } from '@mui/material';
 // three import
 import MainContainer from "components/Three/MainContainer";
+import styles from "./CustomMain.module.css"
 
 
 const CustomMain= () => {
@@ -11,15 +12,21 @@ const CustomMain= () => {
         <Grid xs={0} sm={2} md={3} xl={4} item id="left_div"></Grid>
 
         {/* 메인 콘텐츠 */}
-        <Grid xs={12} sm={8} md={6} xl={4} item container direction="column" id="main_div">
-          <Grid xs={2} item>
-            <h1>CustomMainPage</h1> 
+        <Grid xs={12} sm={8} md={6} xl={4} item id="main_div" container direction="column" justifyContent="space-between">                                
+          {/* 상단 */}
+          <Grid component="div" item xs={2} className={styles.upper}>
+            <h1 className={styles.font_test}>나의 스노우볼</h1>
           </Grid>
-          <Grid xs={9} item>
+
+          {/* Three.js */}
+          <Grid component="div" item xs={9}>
             <MainContainer/>
           </Grid>
-          
 
+          {/* 하단 */}
+          <Grid component="div" item xs={1}>
+            <img src="/images/decoration.png" alt="" className={styles.decoration}/>
+          </Grid>      
         </Grid>
 
         {/* 오른쪽 마진 */}
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../public/static/fonts/style.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
