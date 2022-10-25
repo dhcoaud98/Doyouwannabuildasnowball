f9107acf3c376ac1eef90f169fb8f26f696974df
SSAFY <coryonronda@gmail.com>
2022-10-25 09:03:39 +0900

    1025_main_container

--- frontend/components/Three/MainContainer.js
+++ frontend/components/Three/MainContainer.js
@@ -1,6 +1,6 @@
 import "./MainContainer.module.css"
 import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
+import { Suspense, useEffect, useRef } from "react"
 import { Center, Environment } from "@react-three/drei"
 import Snow_globe from "./Snow_globe"
 import Model from './Scene'
@@ -10,9 +10,17 @@ import { MeshReflectorMaterial } from "@react-three/drei"
 
 function MainContainer() {
 
+  
+
   return (
     <div className='canvas-container'>
-        <Canvas camera={{ position: [0,0,5], fov: 35 }} gl={{ alpha: false }}  id={'menu-canvas'}>
+      <style jsx>{`
+      .canvas-container {
+      width:100%;
+      height:100%;
+      }
+    `}</style>
+        <Canvas width="100" height="400" camera={{ position: [0,0,5], fov: 35 }} gl={{ alpha: false }} dpr={[1,2]} id={'menu-canvas'}>
             <directionalLight intensity={2} position={[10, 6, 6]}> 
             </directionalLight>
             <Suspense fallback={null}>
