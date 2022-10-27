a36010770a0900f707eb1866ff500bda66b8bde7
남성은 <myaeja200@naver.com>
2022-10-27 11:25:53 +0900

    feat: 스피드다이얼 버튼 디자인 끗

--- frontend/pages/CustomMain.tsx
+++ frontend/pages/CustomMain.tsx
@@ -3,6 +3,28 @@ import { Grid, Stack } from '@mui/material';
 import MainContainer from "components/Three/MainContainer";
 import styles from "./CustomMain.module.css"
 
+import { styled } from '@mui/material/styles';
+import SpeedDial, { SpeedDialProps } from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+
+import MenuIcon from '@mui/icons-material/Menu';
+import PeopleIcon from '@mui/icons-material/People';
+import ShareIcon from '@mui/icons-material/Share';
+import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
+import { red } from '@mui/material/colors';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+
+
+const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
+  position: 'absolute',
+}));
+
+const actions = [
+  { icon: <AutoFixHighIcon />, name: '꾸미기' },
+  { icon: <ShareIcon />, name: '공유' },
+  { icon: <PeopleIcon />, name: '친구목록' },
+];
 
 const CustomMain= () => {
   return (
@@ -14,8 +36,47 @@ const CustomMain= () => {
         {/* 메인 콘텐츠 */}
         <Grid xs={12} sm={8} md={6} xl={4} item id="main_div" container direction="column" justifyContent="space-between">                                
           {/* 상단 */}
-          <Grid component="div" item xs={2} className={styles.upper}>
-            <h1 className={styles.font_test}>나의 스노우볼</h1>
+          <Grid component="div" item container xs={2} className={styles.upper}>
+              
+              <Grid xs={2} item component="div"></Grid>
+
+              {/* 현재 화면 이름 */}
+              <Grid xs={8} item component="div" style={{justifyContent: 'end'}}>
+                <h1 className='cntmenu-text'>나의 스노우볼</h1>
+              </Grid>
+
+              {/* 스피드다이얼 */}
+              <Grid xs={2} item component="div" style={{position:'relative', height: '4.5rem'}}>
+
+              {/* <Box component="div" sx={{ position: 'relative', mt: 3, height: 320 }}> */}
+                <StyledSpeedDial
+                  ariaLabel="SpeedDial playground example"
+                  icon={<MenuIcon />}
+                  direction='down'
+                  FabProps={{
+                    sx: {
+                      // bgimage: '/images/wreath_1.png',
+                      bgcolor: '#FFF3E1',
+                      // bgcolor: 'transparent',
+                      color: '#662113',
+                      '&:hover': {
+                        bgcolor: '#FFF3E1',
+                      }
+                    }
+                  }}
+                >
+                  {actions.map((action) => (
+                    <SpeedDialAction
+                      key={action.name}
+                      icon={action.icon}
+                      tooltipTitle={action.name}
+                      className={styles.brownicon}
+                    />
+                  ))}
+                </StyledSpeedDial>
+              {/* </Box> */}
+              </Grid>
+
           </Grid>
 
           {/* Three.js */}
