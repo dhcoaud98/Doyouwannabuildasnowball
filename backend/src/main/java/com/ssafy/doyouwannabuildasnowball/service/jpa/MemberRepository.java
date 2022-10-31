7a7e35ea185993a69ca88add15dd682c78f5346d
고유라 <koyura183982@naver.com>
2022-10-31 16:59:24 +0900

    reset

---
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/service/jpa/MemberRepository.java
@@ -0,0 +1,34 @@
+package com.ssafy.doyouwannabuildasnowball.service.jpa;
+
+import com.ssafy.doyouwannabuildasnowball.domain.Member;
+import com.ssafy.doyouwannabuildasnowball.domain.Snowglobe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByKakaoId(Long kakaoId);
+    Optional<Member> findByEmail(String email);
+    Boolean existsByNickname(String nickname);
+
+    @Modifying
+    @Query("update Member m set m.snowglobe.snowglobeId =:snowglobeId where m.memberId =:memberId")
+        // native query
+        // @Query(value = "UPDATE Member m SET m.snowglobeId=:snowglobeId WHERE m.memberId=:memberId", nativeQuery = true)
+        // JPQL
+    void updateSnowglobeIdById(@Param("memberId") Long memberId,@Param("snowglobeId") Long snowglobeId);
+
+    @Modifying
+    @Query("update Member m set m.nickname =:nickname where m.memberId =:memberId")
+    void updateMemberNickname(@Param("memberId") Long memberId, @Param("nickname") String nickname);
+
+
+    @Modifying
+    @Query("update Member m set m.refreshToken =:refreshToken where m.kakaoId =:kakaoId")
+    void updateRefreshToken(@Param("kakaoId") Long kakaoId, @Param("refreshToken") String refreshToken);
+
+
+}
