888a117067bc6d6df175692ee69a830ff7e0acf1
SSAFY <coryonronda@gmail.com>
2022-11-02 15:23:33 +0900

    feature-front-reduxpersist

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,10 +3,18 @@ import type { AppProps } from 'next/app'
 import '../public/static/fonts/style.css'
 import { Provider } from 'react-redux'
 import store from 'store/store'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+
+let persistor = persistStore(store)
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <Component {...pageProps} />
+    </PersistGate>
   </Provider>
   )
 }
