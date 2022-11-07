8d04c7c3e76b8937c7bc636bb1b93cf2d26ff185
고유라 <koyura183982@naver.com>
2022-11-07 13:49:47 +0900

    fix: global exception handling 추가

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
@@ -1,10 +1,16 @@
 package com.ssafy.doyouwannabuildasnowball.common.exception;
 
+import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
-// hello~
 @RestControllerAdvice
 public class GlobalException {
 
+    @ExceptionHandler(value = Exception.class)
+    public String globalExceptionHandler(Exception e) {
+        String message = new Gson().toJson(e.getMessage());
+        System.out.println("error message: " + message);
+        return message;
+    }
 }
