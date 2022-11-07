8f1dffb8c84110d0292ea5ad07a75fdaf21a6eb2
BaekSeungHoon <psh104404@naver.com>
2022-11-07 13:01:34 +0900

    setting: git test

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
@@ -2,6 +2,8 @@ package com.ssafy.doyouwannabuildasnowball.common.exception;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+// hello~
 @RestControllerAdvice
 public class GlobalException {
 
