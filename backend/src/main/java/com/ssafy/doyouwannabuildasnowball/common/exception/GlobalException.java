01eec9ef8e26f30209546576ddb5a0ed59df3b36
고유라 <koyura183982@naver.com>
2022-11-09 10:39:46 +0900

    feat: add global exception

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/common/exception/GlobalException.java
@@ -1,10 +1,18 @@
 package com.ssafy.doyouwannabuildasnowball.common.exception;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-
-// hello~
 @RestControllerAdvice
+@Slf4j
 public class GlobalException {
+    @ExceptionHandler(value = Exception.class)
+    public String globalExceptionHandler(Exception e) {
+        String message = new Gson().toJson(e.getMessage());
+        log.info("error message: " + message);
+        return message;
+    }
 
 }
