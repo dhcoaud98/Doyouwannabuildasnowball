ba18b62caadc9eae03ebb3f5201b9824653ce02f
SSAFY <coryonronda@gmail.com>
2022-11-10 13:58:06 +0900

    setting_distrib

--- frontend/src/components/three/MainContainer.js
+++ frontend/src/components/three/MainContainer.js
@@ -65,17 +65,16 @@ export const MainContainer = forwardRef((props, ref) => {
       background: linear-gradient(0deg, rgba(246,60,60,1) 0%, rgba(255,120,120,1) 100%);
       }
     `}</style>
-        <Canvas width="100" height="400"  gl={{ preserveDrawingBuffer: true }} dpr={[1,2]} id={'menu-canvas'} ref={target} onClick={() => saveImage('1423')}>
+        <Canvas width="100" height="400"  gl={{ preserveDrawingBuffer: true }} dpr={[1,2]} id={'menu-canvas'} ref={target}>
           <OrbitControls/>
           <directionalLight intensity={2} position={[10, 6, 6]}> 
           </directionalLight>
           <Suspense fallback={null}>  
-            <Center onCentered={({ container, height }) => container.scale.setScalar(0.05)}>
+            <Center onCentered={({ container, height }) => container.scale.setScalar(0.045)}>
                 <Group/>    
             </Center>
             <Environment preset="dawn" background={false} />
           </Suspense>
-          
         </Canvas>
     </div>
     
