77f93655b51b2502e0d8341613392fb6704bf494
SSAFY <coryonronda@gmail.com>
2022-11-16 10:15:27 +0900

    three objet embed all

--- frontend/src/components/three/Group.js
+++ frontend/src/components/three/Group.js
@@ -26,16 +26,6 @@ export function Group() {
   const Building_04 = React.lazy(() => import("./Building/Building_04"))
   const Building_05 = React.lazy(() => import("./Building/Building_05"))
   const Building_06 = React.lazy(() => import("./Building/Building_06"))
-  // Pets import
-  const Pets_01 = React.lazy(() => import("./Pets/Pets_01"))
-  const Pets_02 = React.lazy(() => import("./Pets/Pets_02"))
-  const Pets_03 = React.lazy(() => import("./Pets/Pets_03"))
-  const Pets_04 = React.lazy(() => import("./Pets/Pets_04"))
-  const Pets_05 = React.lazy(() => import("./Pets/Pets_05"))
-  const Pets_06 = React.lazy(() => import("./Pets/Pets_06"))
-  const Pets_07 = React.lazy(() => import("./Pets/Pets_07"))
-  const Pets_08 = React.lazy(() => import("./Pets/Pets_08"))
-  const Pets_09 = React.lazy(() => import("./Pets/Pets_09"))
   // Snowman import
   const Snowman_01 = React.lazy(() => import("./Snowman/Snowman_01"))
   const Snowman_02 = React.lazy(() => import("./Snowman/Snowman_02"))
@@ -55,13 +45,44 @@ export function Group() {
   const Objet1_07 = React.lazy(() => import("./Objet1/Objet1_07"))
   const Objet1_08 = React.lazy(() => import("./Objet1/Objet1_08"))
   const Objet1_09 = React.lazy(() => import("./Objet1/Objet1_09"))
-  // const Objet1_10 = React.lazy(() => import("./Objet1/Objet1_10"))
+  // Objet2 import
+  const Objet2_01 = React.lazy(() => import("./Objet2/Objet2_01"))
+  const Objet2_02 = React.lazy(() => import("./Objet2/Objet2_02"))
+  const Objet2_03 = React.lazy(() => import("./Objet2/Objet2_03"))
+  const Objet2_04 = React.lazy(() => import("./Objet2/Objet2_04"))
+  const Objet2_05 = React.lazy(() => import("./Objet2/Objet2_05"))
+  const Objet2_06 = React.lazy(() => import("./Objet2/Objet2_06"))
+  const Objet2_07 = React.lazy(() => import("./Objet2/Objet2_07"))
+  const Objet2_08 = React.lazy(() => import("./Objet2/Objet2_08"))
+  const Objet2_09 = React.lazy(() => import("./Objet2/Objet2_09"))
+   // Objet3 import
+   const Objet3_01 = React.lazy(() => import("./Objet3/Objet3_01"))
+   const Objet3_02 = React.lazy(() => import("./Objet3/Objet3_02"))
+   const Objet3_03 = React.lazy(() => import("./Objet3/Objet3_03"))
+   const Objet3_04 = React.lazy(() => import("./Objet3/Objet3_04"))
+   const Objet3_05 = React.lazy(() => import("./Objet3/Objet3_05"))
+   const Objet3_06 = React.lazy(() => import("./Objet3/Objet3_06"))
+   const Objet3_07 = React.lazy(() => import("./Objet3/Objet3_07"))
+   const Objet3_08 = React.lazy(() => import("./Objet3/Objet3_08"))
+   const Objet3_09 = React.lazy(() => import("./Objet3/Objet3_09"))
+     // Pets import
+  const Pets_01 = React.lazy(() => import("./Pets/Pets_01"))
+  const Pets_02 = React.lazy(() => import("./Pets/Pets_02"))
+  const Pets_03 = React.lazy(() => import("./Pets/Pets_03"))
+  const Pets_04 = React.lazy(() => import("./Pets/Pets_04"))
+  const Pets_05 = React.lazy(() => import("./Pets/Pets_05"))
+  const Pets_06 = React.lazy(() => import("./Pets/Pets_06"))
+  const Pets_07 = React.lazy(() => import("./Pets/Pets_07"))
+  const Pets_08 = React.lazy(() => import("./Pets/Pets_08"))
+  const Pets_09 = React.lazy(() => import("./Pets/Pets_09"))
 
 
   const c_tree_id = useSelector((state) => state.snowball.deco[0].indicator)
   const building_id = useSelector((state) => state.snowball.deco[1].indicator)
   const snowman_id = useSelector((state) => state.snowball.deco[2].indicator)
   const objet1_id = useSelector((state) => state.snowball.deco[3].indicator)
+  const objet2_id = useSelector((state) => state.snowball.deco[4].indicator)
+  const objet3_id = useSelector((state) => state.snowball.deco[5].indicator)
   const pets_id = useSelector((state) => state.snowball.deco[6].indicator)
 
   console.log(c_tree_id)
@@ -79,6 +100,11 @@ export function Group() {
           {snowman_id == 0 ? <Snowman_01 scale={0.17} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 1 ? <Snowman_02 scale={0.06} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 2 ? <Snowman_03 scale={0.06} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 3 ? <Snowman_04 scale={0.06} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 4 ? <Snowman_05 scale={0.085} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 5 ? <Snowman_06 scale={0.11} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 6 ? <Snowman_07 scale={0.11} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : snowman_id == 7 ? <Snowman_08 scale={0.06} position={[-34,-18, 15]} rotation={[0,19.5,0]}/> : <Pets_09 scale={0.08} position={[-34,-18, 15]} rotation={[0,19.5,0]}/>}
 
           {objet1_id == 0 ? <Objet1_01 scale={0.05} position={[-16,-26, 20]} rotation={[0,19.5,0]}/> : objet1_id == 1 ? <Objet1_02 scale={0.05} position={[-16,-26, 20]} rotation={[0,19.5,0]}/> : objet1_id == 2 ? <Objet1_03 scale={0.05} position={[-16,-26, 20]} rotation={[0,19.5,0]}/> : objet1_id == 3 ? <Objet1_04 scale={0.55} position={[0, 35, 0]} rotation={[0,19.5,0]}/> : objet1_id == 4 ? <Objet1_05 scale={0.2} position={[0, 36, 0]}/> : objet1_id == 5 ? <Objet1_06 scale={0.24} position={[-16,-23, 20]} rotation={[0,19.5,0]}/> : objet1_id == 6 ? <Objet1_07 scale={0.25} position={[0, 36, 0]} /> : objet1_id == 7 ? <Objet1_08 scale={0.045} position={[-16,-26, 20]} rotation={[0,19.5,0]}/> :  objet1_id == 8 ? <Objet1_09 scale={0.08} position={[-13,-24, 18]} rotation={[0,19.5,0]}/> : null}
+        
+          {objet2_id == 0 ? <Objet2_01 scale={0.05} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> : objet2_id == 1 ? <Objet2_02 scale={0.08} position={[16,-26, 20]} rotation={[0,19.5,0]}/> : objet2_id == 2 ? <Objet2_03 scale={0.125} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> : objet2_id == 3 ? <Objet2_04 scale={0.05} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> : objet2_id == 4 ? <Objet2_05 scale={0.06} rotation={[0,-19.5,0]} position={[16,-26, 20]}/> : objet2_id == 5 ? <Objet2_06 scale={0.20} position={[16,-18, 20]} rotation={[0,-19.5,0]}/> : objet2_id == 6 ? <Objet2_07 scale={20} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> : objet2_id == 7 ? <Objet2_08 scale={0.12} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> :  objet2_id == 8 ? <Objet2_09 scale={20} position={[16,-26, 20]} rotation={[0,-19.5,0]}/> : null}
+
+          {objet3_id == 0 ? <Objet3_01 scale={0.04} position={[-26,-8 , 12]} rotation={[0,0,0]}/> : objet3_id == 1 ? <Objet3_02 scale={0.08} position={[-16,-26, 30]} rotation={[0,19.5,0]}/> : objet3_id == 2 ? <Objet3_03 scale={0.125} position={[0,-26, 20]} rotation={[0,-19.5,0]}/> : objet3_id == 3 ? <Objet3_04 scale={0.05} position={[0,-26, 20]} rotation={[0,-19.5,0]}/> : objet3_id == 4 ? <Objet3_05 scale={0.06} rotation={[0,-19.5,0]} position={[0,-26, 20]}/> : objet3_id == 5 ? <Objet3_06 scale={0.20} position={[0,-18, 20]} rotation={[0,-19.5,0]}/> : objet3_id == 6 ? <Objet3_07 scale={20} position={[0,-26, 20]} rotation={[0,-19.5,0]}/> : objet3_id == 7 ? <Objet3_08 scale={0.12} position={[0,-26, 20]} rotation={[0,-19.5,0]}/> :  objet3_id == 8 ? <Objet3_09 scale={20} position={[0,-26, 20]} rotation={[0,-19.5,0]}/> : null}
+
         </Suspense>
     </group>
   )
