48cabcf2f364bf14494713d8ba71a399e23b2be4
SSAFY <coryonronda@gmail.com>
2022-11-17 17:35:20 +0900

    unity save, c_tree null_reference_error, music_change

--- frontend/src/pages/unitybackground.tsx
+++ frontend/src/pages/unitybackground.tsx
@@ -4,14 +4,21 @@ import { useEffect } from "react";
 
 // axios
 import "./unitybackground.css"
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../app/store";
+import axios from "axios";
+
+// else
+import { API_URL } from "../switchurl";
+import { setCurrentSb } from "../features/snowballSlice";
 
 
 function UnityBackground() {
 
   const currentSbId = useSelector((state: RootState) => state.snowball.current_sb_id)
   const deco = useSelector((state: RootState) => state.snowball.deco)
+  const dispatch = useDispatch()
+  const APIURL = API_URL()
   // Unity Embed
   const { unityProvider, sendMessage, addEventListener, removeEventListener, requestFullscreen } = useUnityContext({
     loaderUrl: "Build/Project.loader.js",
@@ -31,21 +38,38 @@ function UnityBackground() {
     };
   }, [addEventListener, removeEventListener,SendRequest])
 
+  useEffect(() => {
+    axios({
+      method:'GET',
+      url:`${APIURL}api/snowglobe/${currentSbId}/detail`
+    })
+    .then((res) => {
+      dispatch(setCurrentSb(res.data))
+    })
+  })
 
   // javascript => unity setMap
   function SendRequest() {
-    console.log('sta')
+    console.log('sta', currentSbId, deco)
     sendMessage("UserObject", "DispatchPos", JSON.stringify({snowglobeid: currentSbId, deco: deco }))
     
   }
 
   function ConMap(pos: string){
     console.log(pos)
+    const payload = JSON.parse(pos)
+    axios({
+      method:"PATCH",
+      url: `${APIURL}api/snowglobe/${payload.snowglobeid}/modifyCoordinate`,
+      data: {deco: payload.deco}
+    })
+    .then((res) => {
+      console.log(res)
+    })
   }
 
   return (
       <div>
-        {/* <MainContainer/> */}
         <Unity unityProvider = {unityProvider}/>
         <button onClick={() => requestFullscreen(true)}>click</button>
       </div>
