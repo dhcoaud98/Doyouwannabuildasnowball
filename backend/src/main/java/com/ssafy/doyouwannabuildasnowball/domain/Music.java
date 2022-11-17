4287786e4d8415241c9830715e9c689710771ca0
고유라 <koyura183982@naver.com>
2022-11-17 16:38:08 +0900

    fix: music id modify

--- backend/src/main/java/com/ssafy/doyouwannabuildasnowball/domain/Music.java
+++ backend/src/main/java/com/ssafy/doyouwannabuildasnowball/domain/Music.java
@@ -14,7 +14,6 @@ import javax.persistence.*;
 public class Music extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "music_id")
     private Long musicId;
 
